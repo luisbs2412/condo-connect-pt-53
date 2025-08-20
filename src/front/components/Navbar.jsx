6ec40db4ec615f80ecc166019846a6f0b81083d6
Liamel Pardo <liamelpardo92@gmail.com>
2025-08-20 02:02:14 +0000

    Navbar Creado

--- src/front/components/Navbar.jsx
+++ src/front/components/Navbar.jsx
@@ -9,21 +9,7 @@ export const Navbar = () => {
         <a className="navbar-brand" href="#">
          CondoConnect
         </a>
-
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-
         <div className="collapse navbar-collapse" id="navbarNav">
-          {/* Links y botón Login alineados a la derecha */}
           <div className="d-flex ms-auto align-items-center">
             <a className="nav-link me-3 active" aria-current="page" href="#">
               Home
