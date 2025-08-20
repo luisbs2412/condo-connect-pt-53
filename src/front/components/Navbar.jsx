fe67b030d79780869c61124df129f8106520006e
Liamel Pardo <liamelpardo92@gmail.com>
2025-08-20 01:58:10 +0000

    Navbar creado

--- src/front/components/Navbar.jsx
+++ src/front/components/Navbar.jsx
@@ -3,20 +3,44 @@ import RigoBaby from "../assets/img/rigo-baby.jpg";
 import icono_light from "../assets/img/icono_light.png";
 
 export const Navbar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="#">
+         CondoConnect
+        </a>
 
-	return (
-		<nav className="navbar"  style={{"backgroundColor": "#f4f5f6"}}>
-			<div className="container">
-				<Link to="/">
-					{/* <span className="navbar-brand mb-0 h1">CondoConect</span> */}
-					<img src={icono_light}  alt="" />
-				</Link>
-				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-sm btn-primary">Login</button>
-					</Link>
-				</div>
-			</div>
-		</nav>
-	);
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="collapse navbar-collapse" id="navbarNav">
+          {/* Links y botón Login alineados a la derecha */}
+          <div className="d-flex ms-auto align-items-center">
+            <a className="nav-link me-3 active" aria-current="page" href="#">
+              Home
+            </a>
+            <a className="nav-link me-3" href="#">
+              About
+            </a>
+            <a className="nav-link me-3" href="#">
+              Contact
+            </a>
+
+            <Link to="/login">
+              <button className="btn btn-primary">Login</button>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
 };
