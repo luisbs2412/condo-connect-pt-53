71b1b2b2969c6a749765f5b03c9aeea075bf9d8b
jhuequeman <huequeman@gmail.com>
2025-08-31 01:26:33 +0000

    Formularios de reserva

--- src/api/models.py
+++ src/api/models.py
@@ -17,3 +17,30 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+    
+class Reservation(db.Model):
+    id:Mapped[int]= mapped_column(primary_key=True)
+    first_name: Mapped[str] = mapped_column(String(50), nullable=False)
+    type:Mapped[str]=mapped_column(unique=True, nullable = False)
+    email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+
+    
+    def serialize(self):
+        return {
+            "id": self.id,
+            "email": self.email,
+            "type": self.type
+            # do not serialize the password, its a security breach
+        }
+
+# Reservation
+# -
+# id int PK
+# type string
+# date date
+# time_start time
+# time_end time
+# status string
+# updated_at string
+# tenant_id int FK >- TenantProfile.id
+# created_at datetime
--- src/api/routes.py
+++ src/api/routes.py
@@ -11,6 +11,7 @@ api = Blueprint('api', __name__)
 # Allow CORS requests to this API
 CORS(api)
 
+reservas = []
 
 @api.route('/hello', methods=['POST', 'GET'])
 def handle_hello():
@@ -20,3 +21,47 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+
+@api.route("/user/listreservas", methods=["GET"])
+def listar_reservas():
+   # return 'estoy conectado', 200
+    return jsonify(reservas), 200
+
+# POST → crear nueva reserva
+@api.route("/user/reservas", methods=["POST"])
+def crear_reserva():
+    data = request.get_json()
+    data["id"] = len(reservas) + 1  # asignamos un ID automático
+    reservas.append(data)
+    return jsonify({
+        "message": "Reserva creada ✅",
+        "reserva": data
+    }), 201
+
+
+
+
+# PUT → actualizar una reserva por id
+
+
+@api.route("/user/reservas/<int:id>", methods=["PUT"])
+def actualizar_reserva(id):
+    data = request.get_json()
+    for r in reservas:
+        if r["id"] == id:
+            r.update(data)
+            return jsonify({
+                "message": "Reserva actualizada ✅",
+                "reserva": r
+            }), 200
+    return jsonify({"error": "Reserva no encontrada"}), 404
+
+# DELETE → eliminar una reserva por id
+
+
+@api.route("/api/reservas/<int:id>", methods=["DELETE"])
+def eliminar_reserva(id):
+    global reservas
+    reservas = [r for r in reservas if r["id"] != id]
+    return jsonify({"message": "Reserva eliminada ✅"}), 200
